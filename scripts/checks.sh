9fb45a46e7a6dbbdc648ce8c02328d17264570b8
CERZAR <stavropoltsev.t@gmail.com>
2019-12-06 19:03:51 +0300

    edited

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -8,7 +8,7 @@ echo "$files" | xargs cpplint --filter=$filter
 
 export CTEST_OUTPUT_ON_FAILURE=true
 # address sanitizer
-#CMAKE_LINKER_OPTS="-DCMAKE_EXE_LINKER='-fuse-ld=gold'"
+CMAKE_LINKER_OPTS="-DCMAKE_EXE_LINKER='-fuse-ld=gold'"
 CMAKE_CONFIG_OPTS="-DHUNTER_CONFIGURATION_TYPES=Debug -DCMAKE_BUILD_TYPE=Debug"
 # shellcheck disable=SC2089
 # shellcheck disable=SC2006
@@ -18,8 +18,9 @@ CMAKE_OPTS="$CMAKE_LINKER_OPTS $CMAKE_CONFIG_OPTS $CMAKE_TOOLCHAIN_OPTS"
 cmake -H. -B_builds/sanitize-address-cxx17 "$CMAKE_OPTS"
 cmake --build _builds/sanitize-address-cxx17
 ./_builds/sanitize-address-cxx17/tests
+
 # thread sanitizer
-CMAKE_LINKER_OPTS="-DCMAKE_EXE_LINKER='-fuse-ld=gold'"
+CMAKE_CONFIG_OPTS="-DHUNTER_CONFIGURATION_TYPES=Debug -DCMAKE_BUILD_TYPE=Debug"
 # shellcheck disable=SC2089
 # shellcheck disable=SC2006
 CMAKE_TOOLCHAIN_OPTS="-DCMAKE_TOOLCHAIN_FILE='`pwd`/tools/polly/sanitize-thread-cxx17-pic.cmake'"
